5bc1a054a6bee6112a0756f5098d187ce79532e6
Emmett Agnew <eragnew@gmail.com>
2015-09-29 08:58:22 -0700

    Created simple Angular shell app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/students_dev');
 process.env.APP_SECRET = process.env.APP_SECRET || 'forrealchangemechangement';
 
+app.use(express.static(__dirname + '/build'));
 var studentsRouter = require(__dirname + '/routes/students_routes');
 var usersRouter = require(__dirname + '/routes/users_routes');
 app.use('/api', studentsRouter);
